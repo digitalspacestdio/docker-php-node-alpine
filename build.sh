ff99e77e7adba6579c0a6da42739170366110e00
scherepanov <sergey@cherepanov.org.ua>
2022-08-12 03:48:06 +0300

    get version from php.net

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
 REPOSITORY=${1-digitalspacestudio}
 PHP_VERSIONS="7.4 8.0 8.1 8.2"
 NODE_VERSIONS="14 16 18"
-ALL_NODE_VERSIONS=$(curl https://unofficial-builds.nodejs.org/download/release/ | pup 'a text{}' | grep -o '[0-9]\+.[0-9]\+.[0-9]\+' | sort --version-sort -r);
+ALL_NODE_VERSIONS=$(curl -L --silent https://unofficial-builds.nodejs.org/download/release/ | pup 'a text{}' | grep -o '[0-9]\+.[0-9]\+.[0-9]\+' | sort --version-sort -r);
 for PHP_VERSION in $PHP_VERSIONS; do
-    exact_php_version=$(docker --log-level error run --rm  php:${PHP_VERSION}-fpm-alpine sh -c "php --version | grep -o '^PHP [0-9]\+\.[0-9]\+\.[0-9A-Za-z]\+' | grep -o '[0-9]\+\.[0-9]\+\.[0-9A-Za-z]\+'")
+    RELEASES=$(curl -L --silent 'https://www.php.net/releases?json&version='$PHP_VERSION)
+    exact_php_version=$(echo ${RELEASES} | jq -r '."version"')
+    #exact_php_version=$(docker --log-level error run --rm  php:${PHP_VERSION}-fpm-alpine sh -c "php --version | grep -o '^PHP [0-9]\+\.[0-9]\+\.[0-9A-Za-z]\+' | grep -o '[0-9]\+\.[0-9]\+\.[0-9A-Za-z]\+'")
     php_major=$(echo $exact_php_version | awk -F. '{print $1}')
     php_minor=$(echo $exact_php_version | awk -F. '{print $2}')
     php_fix=$(echo $exact_php_version | awk -F. '{print $3}')
